acfe9d9f35e49a9ccf1b1afd1802f87ff5cbfbce
awakevwake <awakevwake@outlook.com>
2022-10-12 21:17:49 +0530

    Added Material UI library

---
+++ src/app/provision/provision.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-provision',
+  templateUrl: './provision.component.html',
+  styleUrls: ['./provision.component.scss']
+})
+export class ProvisionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

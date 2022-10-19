0b220f7d01216382d4e55f9e4a4a025354903a76
awakevwake <awakevwake@outlook.com>
2022-10-19 23:45:37 +0530

    added grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Options {
+  value: string;
+  viewValue: string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +14,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Post-man-lite-app';
   opened = false;
+
+
+  foods: Options[] = [
+    {value: '0', viewValue: 'Steak'},
+    {value: '1', viewValue: 'Pizza'},
+    {value: '2', viewValue: 'Tacos'},
+  ];
+
+  places: Options[] = [
+    {value: '0', viewValue: 'New York'},
+    {value: '1', viewValue: 'Ohio'},
+    {value: '2', viewValue: 'Texas'},
+  ];
+
+  sports: Options[] = [
+    {value: '0', viewValue: 'Soccer'},
+    {value: '1', viewValue: 'Rugby'},
+    {value: '2', viewValue: 'Baseball'},
+  ];
+
 }
